ebcdb6a8951ece3c14c10a445f62303c22d904ae
Dylan Brown <dylanbrown588@gmail.com>
2024-03-18 14:06:16 -0500

    Make the budget router

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const passport = require('./strategies/user.strategy');
 
 // Route Includes
 const userRouter = require('./routes/user.router');
+const budgetRouter = require('./routes/budget.router');
+const businessRouter = null;
 
 // Express Middleware
 app.use(express.json());
@@ -24,6 +26,7 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
+app.use('/api/budget', budgetRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
