c68320ff25468e00723b520c6398b784b19378d1
Seid Redi <seidredi7@gmail.com>
2024-03-18 14:42:27 -0500

    set up business route

---
+++ server/routes/business.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET: get businesses for a particular user
+ */
+router.get('/', (req, res) => {
+  
+});
+
+/**
+ * POST: create business
+ */
+router.post('/', (req, res) => {
+  
+});
+
+module.exports = router;
