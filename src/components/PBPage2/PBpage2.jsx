ddfee5f6d54d709092fddfd1728f2f9a86633741
Dylan Brown <dylanbrown588@gmail.com>
2024-03-19 14:15:55 -0500

    Handle input change

--- src/components/PBPage2/PBpage2.jsx
+++ src/components/PBPage2/PBpage2.jsx
@@ -8,7 +8,7 @@ function StartPlan(props) {
         carInsurance: 0,
         houseInsurance: 0,
         creditCard: 0,
-        loanPaymeant: 0
+        loanPayment: 0
     });
 
     const handleInputChange = (event) => {
@@ -33,17 +33,16 @@ function StartPlan(props) {
                 <Typography variant="subtitle1" align="center" gutterBottom sx={{ marginBottom: 2 }}>
                     Take some time to think about the monthy bill payments you have to pay.
                 </Typography>
-
                 <form onSubmit={handleSubmit}>
                     <Grid container spacing={2} justifyContent="center">
                         <Grid item xs={12} md={6}>
-                            <TextField name="realEstateTaxes" label="Real Estate Taxes" fullWidth value={formValues.realEstateTaxes} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                            <TextField name="carInsurance" label="Car Insurance" fullWidth value={formValues.carInsurance} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                            <TextField name="houseInsurance" label="House Insurance" fullWidth value={formValues.houseInsurance} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="realEstateTaxes" label="Real Estate Taxes" fullWidth value={formValues.realEstateTaxes} onChange={(event) => setFormValues({...formValues, realEstateTaxes: event.target.value})} sx={{ marginBottom: 2 }} />
+                            <TextField name="carInsurance" label="Car Insurance" fullWidth value={formValues.carInsurance} onChange={(event) => setFormValues({...formValues, carInsurance: event.target.value})} sx={{ marginBottom: 2 }} />
+                            <TextField name="houseInsurance" label="House Insurance" fullWidth value={formValues.houseInsurance} onChange={(event) => setFormValues({...formValues, houseInsurance: event.target.value})} sx={{ marginBottom: 2 }} />
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <TextField name="creditCard" label="All Credit Card Payment" fullWidth value={formValues.creditCard} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                            <TextField name="loanPaymeant" label="All Loan Payments" fullWidth value={formValues.loanPaymeant} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="creditCard" label="All Credit Card Payment" fullWidth value={formValues.creditCard} onChange={(event) => setFormValues({...formValues, creditCard: event.target.value})} sx={{ marginBottom: 2 }} />
+                            <TextField name="loanPayment" label="All Loan Payments" fullWidth value={formValues.loanPayment} onChange={(event) => setFormValues({...formValues, loanPaymeant: event.target.value})} sx={{ marginBottom: 2 }} />
                         </Grid>
                     </Grid>
                     <Box textAlign="center" marginTop={4}>
