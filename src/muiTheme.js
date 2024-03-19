75e99136e6e83fc5decec68fae289741eec09e95
Gus Ridgway <auridgway@gmail.com>
2024-03-19 16:18:35 -0500

    added some styling changes

--- src/muiTheme.js
+++ src/muiTheme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
         primary: {
-            main: 'rgb(75, 168, 94)', // Primary color
+            main: 'rgb(22, 147, 171)', // Primary color
         },
         secondary: {
             main: 'rgb(208, 50, 39)', // Secondary color
