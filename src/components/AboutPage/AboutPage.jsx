3fa8ff67ab13b580bdc5b2030138b1d620b279c9
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-03-18 23:11:37 -0500

    Created and imported a mui theme, tested out on the about page at the moment.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import theme from '../../muiTheme'; // Import the custom theme
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -9,9 +11,23 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>Make a DAMN Plan!</p>
+        <p>https://www.youtube.com/watch?v=DLHSVnvK3Is</p>
       </div>
+      <div>
+      <Button variant="contained" color="primary">
+        Primary Button
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary Button
+      </Button>
+      {/* You can use your third color as well */}
+      <Button variant="contained" style={{ backgroundColor: theme.palette.third.main }}>
+        Third Button
+      </Button>
     </div>
+    </div>
+    
   );
 }
 
