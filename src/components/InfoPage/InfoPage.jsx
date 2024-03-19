b334a1d46da6e39e1c99668901e64c97c9458965
Dylan Brown <dylanbrown588@gmail.com>
2024-03-18 21:02:29 -0500

    Add steps to info page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -3,6 +3,7 @@ import { Typography, Card, CardContent, Stack } from '@mui/material';
 
 function InfoPage() {
   return (
+    <>
     <Card>
       <CardContent>
         <Typography variant="h4" gutterBottom>
@@ -46,6 +47,51 @@ function InfoPage() {
         </Typography>
       </CardContent>
     </Card>
+    <Card>
+    <CardContent>
+      <Typography variant="h4" gutterBottom>
+        Personal Budget: Three Steps To Determine Your Value Pay
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Step One: Enter in the amount spent per month on each line item If you have not taken the time to analyze your exact budget, now is a good time to do it.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Step Two: Enter the percent of household expenses that you must cover in your household. Example: If you are the only income in your household, enter 100%. If you share expenses with another person, or receive support to assist with monthly household expenses, enter the percentage that you must cover with cash income.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Step Three: Calculate your added Value Compensation. This is the amount of money, on top of the percentage of household and living expenses that the business must cover including income taxes. This is the amount of money that you want to get paid - the amount that expresses your value.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Once you've completed these steps, you'll have a clearer understanding of your value pay and how it fits into your personal budget.
+      </Typography>
+    </CardContent>
+  </Card>
+  <Card>
+      <CardContent>
+        <Typography variant="h4" gutterBottom>
+          Marketing Plan: Six Steps To Prepare Your Marketing Budget
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Step One: Once you have completed your market research and made determined-decisions about your marketing strategy and the marketing tools that you will use, itemize the cost of each tool or marketing activity. Enter each activity in the Marketing Tools/Activities Column. If the marketing expense is already part of an employee's time or covered as a contractor fee, do not show the cost in the Table, but include the activity to document the task getting done.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Step Two: For each item identify the frequency and timing of the activity or tool.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Step Three: Identify who will be responsible for conducting the activities or producing the tools described. Are they a contractor or In-house talent? If you are not sure which is best, conduct a thorough time and cost/benefit analysis.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Step Four: What assets such as copy, photos, pricing information, links, etc will be needed to successfully deploy this marketing activity. Be sure to factor in the time it takes, resources and cost to put all assets together.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Step Five: Enter the cost per use or deployment for each item. Example: If it costs $20 for each ad and you plan to run the ad 12 times per month. Enter $20 in the Cost per Use Column and 12 in the Monthly Frequency Column. For items like promotional items or business cards, enter a monthly budget with a frequency of 1. Development of an initial website, signage or other large in frequent expenses will be itemized in start-up expenses.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Step Six: For each marketing tool identify a specific and expected result. Examples: Increase customer loyalty as evidenced by repeat purchases. Increase sales in specific dollar amounts. Establish brand as evidenced by online rankings.
+        </Typography>
+      </CardContent>
+    </Card>
+  </>
   );
 }
 
