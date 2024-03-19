f97cfa8365b04550fdfbbd6a73ca129e391842f7
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-03-19 17:25:45 -0500

    wired in the budget saga to root saga, and the budget reducer _root.reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import business from './business.reducer';
+import budget from './budget.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +13,8 @@ import business from './business.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  business // Gets the businesses for the logged in user
+  business, // Gets the businesses for the logged in user
+  budget // Gets the budgets for the logged in use per their business
 });
 
 export default rootReducer;
