3d97c1ea4cdcbee66639fa37ec7efde1337118f5
Dylan Brown <dylanbrown588@gmail.com>
2024-03-19 13:31:46 -0500

    Make business reducer and saga

--- src/components/PBPage2/PBpage2.jsx
+++ src/components/PBPage2/PBpage2.jsx
@@ -1,15 +1,66 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Box, Typography, TextField, Button, Container, Grid, Paper } from '@mui/material';
 
+function StartPlan(props) {
+    const store = useSelector((store) => store);
 
-function TemplateFunction(props) {
+    const [formValues, setFormValues] = useState({
+        rentOrMortgage: '',
+        electric: '',
+        heat: '',
+        water: '',
+        internet: '',
+        telephone: '',
+        childcare: '',
+    });
 
-  return (
-    <div>
-      <h2>Regular Financial Responsibilities</h2>
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
 
-    </div>
-  );
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Form Values: ', formValues);
+    };
+
+    return (
+        <Container maxWidth="md">
+            <Paper elevation={3} style={{ padding: 24, marginTop: 32 }}>
+                <Typography variant="h4" align="center" gutterBottom>
+                    Fundamental Living Expenses
+                </Typography>
+                <Typography variant="subtitle1" align="center" gutterBottom sx={{ marginBottom: 2 }}>
+                    Your singular goal in business is to "meet your customer's wants and needs at a profit" and pay yourself!
+                </Typography>
+
+                <form onSubmit={handleSubmit}>
+                    <Grid container spacing={2} justifyContent="center">
+                        <Grid item xs={12} md={6}>
+                            <TextField name="rentOrMortgage" label="Rent or Mortgage" fullWidth value={formValues.rentOrMortgage} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="electric" label="Electric" fullWidth value={formValues.electric} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="heat" label="Heat" fullWidth value={formValues.heat} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="water" label="Water" fullWidth value={formValues.water} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <TextField name="internet" label="Internet" fullWidth value={formValues.internet} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="telephone" label="Telephone" fullWidth value={formValues.telephone} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                            <TextField name="childcare" label="Childcare" fullWidth value={formValues.childcare} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
+                        </Grid>
+                    </Grid>
+                    <Box textAlign="center" marginTop={4}>
+                        <Button type="submit" variant="contained" color="primary">
+                            Next Page
+                        </Button>
+                    </Box>
+                </form>
+            </Paper>
+        </Container>
+    );
 }
 
-export default TemplateFunction;
+export default StartPlan;
