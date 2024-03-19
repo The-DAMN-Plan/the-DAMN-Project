22107402c0091e62c804f129157d7c2f13c4460a
Dylan Brown <dylanbrown588@gmail.com>
2024-03-19 11:47:53 -0500

    Start the page 2 of the damn plan questions

---
+++ src/components/PBPage2/PBpage2.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
+
+
+function TemplateFunction(props) {
+
+  return (
+    <div>
+      <h2>Regular Financial Responsibilities</h2>
+
+    </div>
+  );
+}
+
+export default TemplateFunction;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -5,6 +5,7 @@ import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function UserPage() {
+  const history = useHistory();
   const user = useSelector((store) => store.user);
   // Will use with the business reducer
   const business = null; 
@@ -14,43 +15,43 @@ function UserPage() {
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       {/* Will make reducers at a later time and style with seeded data */}
-    <Container maxWidth="lg">
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Paper style={{ maxHeight: '400px', overflowY: 'auto' }}>
-            <Box p={3}>
-              <Typography variant="h5" gutterBottom>
-                Business Users
-              </Typography>
-              <Typography variant="body1">
-                Business 1
-              </Typography>
-              <Typography variant="body1">
-                Business 2
-              </Typography>
-            </Box>
-          </Paper>
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            {/* Will need a loop to loop over and display businesses */}
+            <Paper style={{ maxHeight: '400px', overflowY: 'auto' }}>
+              <Box p={3}>
+                <Typography variant="h5" gutterBottom>
+                  Business Users
+                </Typography>
+                <Typography variant="body1">
+                  Business 1
+                </Typography>
+                <Typography variant="body1">
+                  Business 2
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper>
+              <Box p={3}>
+                <Typography variant="h5" gutterBottom>
+                  Budgets
+                </Typography>
+                <Box display="flex" justifyContent="space-between" marginBottom="10px">
+                  <Button variant="contained" color="primary">
+                    Start a New Plan
+                  </Button>
+                  <Button variant="contained" color="primary" onClick={() => { history.push('/info')}}>
+                    Info
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Paper>
-            <Box p={3}>
-              <Typography variant="h5" gutterBottom>
-                Budgets
-              </Typography>
-              <Button variant="contained" color="primary" style={{ marginBottom: '10px' }}>
-                Start a New Plan
-              </Button>
-            </Box>
-          </Paper>
-        </Grid>
-        {/* Logout Button */}
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary">
-            Info
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
     </div>
   );
 }
