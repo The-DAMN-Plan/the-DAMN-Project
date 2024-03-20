aa35dc8f77857b2135c2918e4210bf237de42f3c
Gus Ridgway <auridgway@gmail.com>
2024-03-20 13:56:35 -0500

    styled the login/register pages

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { TextField } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector(store => store.errors);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const login = (event) => {
     event.preventDefault();
@@ -25,41 +32,75 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+    <Grid container>
+      <Grid xs={2} >
+
+      </Grid>
+      <Grid component='form' direction='column' autoComplete="off" alignItems="center" justifyContent="center" container spacing={2} xs={8} onSubmit={login} >
+        <Paper sx={{p: 3, m:3}}>
+          <Grid textAlign='center'>
+            <Typography variant='h5' sx={{mb:1}}>Login</Typography>
+            {errors.loginMessage && (
+              <Typography variant='h6' className="alert" role="alert">
+                {errors.loginMessage}
+              </Typography>
+            )}
+          </Grid>
+
+          <Grid>
+            <TextField
+              fullWidth label='Username' variant="outlined"
+              type="text"
+              name="username"
+              sx={{ my: 0.5 }}
+              required
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </Grid>
+          <Grid>
+            <TextField
+              fullWidth label='Password'
+              variant="outlined"
+              type="password"
+              name="password"
+              sx={{ my: 0.5 }}
+              required
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Grid>
+          <Grid>
+            <Button fullWidth variant='contained'
+              className="btn"
+              type="submit"
+              name="submit"
+              value="Log In"
+              sx={{ my: 0.5 }}
+            >
+              Log In
+            </Button>
+
+            <Button fullWidth
+              variant='outlined'
+              color='secondary'
+              type="button"
+              className="btn btn_asLink"
+              sx={{ my: 0.5 }}
+              onClick={() => {
+                history.push('/registration');
+              }}
+            >
+              Register
+            </Button>
+          </Grid>
+
+        </Paper>
+      </Grid>
+      <Grid xs={2} >
+
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-
-import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
+import { Container } from '@mui/material';
 
 function RegisterPage() {
-  const history = useHistory();
 
   return (
-    <div>
+    <Container>
       <RegisterForm />
-
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          Login
-        </button>
-      </center>
-    </div>
+    </Container>
   );
 }
 
