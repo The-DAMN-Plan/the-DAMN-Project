521709336da57b16d43b9ce9473d0abe7bdbd2a6
Dylan Brown <dylanbrown588@gmail.com>
2024-03-19 21:58:50 -0500

    Make starting files for buget redux and start expense saga

---
+++ src/redux/sagas/expense.saga.js
@@ -0,0 +1,14 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* addPersonalExpense(action) {
+    try {
+        yield axios.post(`/api/budget/expense`, action.payload);
+    } catch(error) {
+        console.log('Error adding personal expense', error);
+    }
+}
+
+function* expenseSaga() {
+    yield takeLatest('ADD_PERSONAL_EXPENSE', addPersonalBusiness);
+}
