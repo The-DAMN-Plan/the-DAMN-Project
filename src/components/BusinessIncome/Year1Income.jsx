fe44af06b804e8d066b3b8c535d81a13d3f290a8
Dylan Brown <dylanbrown588@gmail.com>
2024-03-21 15:25:15 -0500

    Make the state for business income page

--- src/components/BusinessIncome/Year1Income.jsx
+++ src/components/BusinessIncome/Year1Income.jsx
@@ -1,68 +1,97 @@
 import React, { useState } from 'react';
 import { TextField, Button, Container, Table, TableBody, TableCell, TableHead, TableRow, Paper, Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import { useDispatch, useSelector } from 'react-redux';
 import ProgressBar from '../ProgressBar/ProgressBar';
 
 
 
 function Year1Income() {
-  const budget = useSelector((store) => store.budget);
-  const budgetObj = budget[0];
+    const budget = useSelector((store) => store.budget);
+    const budgetObj = budget[0];
+
+    const [productName, setProductName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [unit, setUnit] = useState('');
+    const [timeUsed, setTimeUsed] = useState('');
+    const [idealClient, setIdealClient] = useState('');
+    const [rateOfLove, setRateOfLove] = useState('');
+    const [purchasers, setPurchasers] = useState('');
+    const [revenueStreams, setRevenueStreams] = useState([]);
+
+  const handleAddRevenueStream = () => {
+    // Validate fields before adding to revenueStreams array
+    if (revenueStream && price && timeUsed) {
+      const newRevenueStream = {
+        revenueStream,
+        description,
+        price,
+        unit,
+        timeUsed,
+        idealClient
+      };
+      setRevenueStreams([...revenueStreams, newRevenueStream]);
+      // Reset fields after submission
+      setProductName('');
+      setDescription('');
+      setPrice('');
+      setUnit('');
+      setTimeUsed('');
+      setIdealClient('');
+      setRateOfLove('');
+      setPurchasers('');
+    }
+  };
   
 
   return (
-    <Container>
-        <Typography
-        textAlign="center"
-        variant='h2'
-        color='primary'>Year 1 Business Income</Typography>
-        <Typography
-        textAlign="center"
-        variant='subtitle1'>Take some time to plan out what services you plan to offer in your first year of business.</Typography>
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container
-            spacing={2}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ minHeight: '30vh' }}
-        >
+    <Container sx={{ paddingTop: '64px' }}> {/* Adjust this value based on the height of your nav bar */}
+    <Typography variant="h1" gutterBottom>
+        Year 1 Businness income
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+        Take some time to think about some services you'll offer in year one of your business.
+    </Typography>
+    <TextField label="Name of Product" value={productName} onChange={(e) => setProductName(e.target.value)} />
+    <TextField label="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+    <TextField label="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
+    <TextField label="Unit" value={unit} onChange={(e) => setUnit(e.target.value)} />
+    <TextField label="Time Used" value={timeUsed} onChange={(e) => setTimeUsed(e.target.value)} />
+    <TextField label="Ideal Client" value={idealClient} onChange={(e) => setIdealClient(e.target.value)} />
+    <TextField label="Rate of Love" value={rateOfLove} onChange={(e) => setRateOfLove(e.target.value)} />
+    <TextField label="Purchasers" value={purchasers} onChange={(e) => setPurchasers(e.target.value)} />
+    <Button onClick={handleAddExpense}>Submit</Button>
 
-          {/* {this is the center spot} */}
-            <Grid container xs={12}>
-            <Box sx={{ mt: 4 }}>
-                <TextField label="Name of Expense" value={expenseName} onChange={(e) => setExpenseName(e.target.value)} fullWidth sx={{ mb: 2 }} />
-                <TextField label="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} fullWidth sx={{ mb: 2 }} />
-                <Button onClick={handleAddExpense} variant="contained" color="primary">Submit</Button>
-
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name of Expense</TableCell>
-                            <TableCell>Amount</TableCell>
-                            <TableCell>Delete</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {expenses.map((expense, index) => (
-                            <TableRow key={index}>
-                                <TableCell>{expense.name}</TableCell>
-                                <TableCell>{`$${expense.amount}`}</TableCell>
-                                <TableCell>
-                                    <Button onClick={() => handleDeleteExpense(index)} variant="outlined" color="secondary">Delete</Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Box>
-        </Grid>
-          {/* {end} */}
-        </Grid>
-        <ProgressBar next={'futureplans'} back={'incomeyear2'} value={75} />
-        </Box>
-    </Container>
-  );
+    <Table>
+        <TableHead>
+            <TableRow>
+                <TableCell>Name of Expense</TableCell>
+                <TableCell>Amount</TableCell>
+                <TableCell>Delete</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {revenueStreams.map((stream, index) => (
+                <TableRow key={index}>
+                    <TableCell>{stream.productName}</TableCell>
+                    <TableCell>{stream.description}</TableCell>
+                    <TableCell>{stream.price}</TableCell>
+                    <TableCell>{stream.unit}</TableCell>
+                    <TableCell>{stream.timeUsed}</TableCell>
+                    <TableCell>{stream.idealClient}</TableCell>
+                    <TableCell>{stream.rateOfLove}</TableCell>
+                    <TableCell>{stream.purchasers}</TableCell>
+                    <TableCell>
+                        <Button onClick={() => handleDeleteExpense(index)}>Delete</Button>
+                    </TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    </Table>
+    <ProgressBar back={''} next={''} value={5}/>
+</Container>
+);
 }
 
 export default Year1Income;
