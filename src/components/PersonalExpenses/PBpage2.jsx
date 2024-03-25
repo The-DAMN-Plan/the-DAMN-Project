75a4ae8a604df74251d16867c406bba68ff91556
Dylan Brown <dylanbrown588@gmail.com>
2024-03-25 15:12:49 -0500

    Get submit working for page 1 and 2

--- src/components/PersonalExpenses/PBpage2.jsx
+++ src/components/PersonalExpenses/PBpage2.jsx
@@ -1,30 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Typography, TextField, Button, Container, Grid, Paper } from '@mui/material';
-import ProgressBar from '../ProgressBar/ProgressBar';
+import { Typography, TextField, Button, Container, Grid } from '@mui/material';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import SideNav from '../Nav/SideNav';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
-function PBPage2() {
+function PBpage2() {
     const dispatch = useDispatch();
-    const budget = useSelector((store) => store.budget);
     const budgetId = useParams();
-    const budgetObj = budget[0];
-    const [userEntry, setUserEntry] = useState([])
-
+    const finalBudget = useSelector((store) => store.finalBudget);
+    const expense = useSelector((store) => store.expense);
+    const [formSubmitted, setFormSubmitted] = useState(false);
     const [formValues, setFormValues] = useState({
-        realEstateTaxes: '',
+        realEstateTax: '',
         carInsurance: '',
         houseInsurance: '',
         creditCard: '',
-        loanPayment: ''
+        loanPayments: ''
     });
+
+    console.log(formValues);
+
+    console.log('Expense reducer', expense);
+    console.log('URL', budgetId);
+    console.log('Big budget object', finalBudget);
+
+    useEffect(() => {
+        dispatch({ type: 'BUDGET_PLAN', payload: budgetId.budgetId });
+    }, [dispatch, budgetId]);
+
+    useEffect(() => {
+        handleExpense();
+    }, [expense]); // Call handleExpense whenever expense changes
+
+    const handleExpense = () => {
+        const newFormValues = {
+            realEstateTax: getExpenseAmount('realEstateTax'),
+            carInsurance: getExpenseAmount('carInsurance'),
+            houseInsurance: getExpenseAmount('houseInsurance'),
+            creditCard: getExpenseAmount('creditCard'),
+            loanPayments: getExpenseAmount('loanPayments')
+        };
+        setFormValues(newFormValues);
+        console.log('Here be new values', newFormValues);
+    };
+
+    const getExpenseAmount = (expenseName) => {
+        const expenseItem = expense.find(item => item.expense_name === expenseName);
+        return expenseItem ? expenseItem.expense_amount : '';
+    };
+
+
+
+    const [userEntry, setUserEntry] = useState([])
+
+
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
-    
+
         // Find the index of the existing formData object with the same expense_name
         const existingIndex = userEntry.findIndex(item => item.expense_name === name);
-    
+
         // If the formData object exists, update its expense_amount
         if (existingIndex !== -1) {
             const updatedUserEntry = [...userEntry];
@@ -36,53 +73,94 @@ function PBPage2() {
         } else {
             // If the formData object doesn't exist, create a new one
             const formData = {
-                budget_id: budgetObj.id,
+                budget_id: Number(budgetId.budgetId),
                 type: 'personal committed',
                 expense_name: name,
                 expense_amount: value
             };
             setUserEntry([...userEntry, formData]);
         }
-    
+
         // Update the form values
         setFormValues({
             ...formValues,
             [name]: value,
         });
     };
+
     console.log(userEntry);
 
     const handleSubmit = (event) => {
-        event.preventDefault()
+        event.preventDefault();
+
         dispatch({ type: 'ADD_PERSONAL_EXPENSE', payload: userEntry });
+        // dispatch({type: 'UPDATE_STATUS', payload: ''}) // Will need to be set up later to post the completed step to the status table
+        setFormSubmitted(true);
     };
 
+    const handleEdit = (event) => {
+        console.log('Edit MAMA');
+    }
     return (
-        <Container maxWidth="md">
-            <Paper elevation={3} style={{ padding: 24, marginTop: 32 }}>
-                <Typography variant="h4" align="center" gutterBottom>
-                    Fundamental Living Expenses
-                </Typography>
-                <Typography variant="subtitle1" align="center" gutterBottom sx={{ marginBottom: 2 }}>
-                    Take some time to think about the monthy bill payments you have to pay.
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <Grid container spacing={2} justifyContent="center">
-                        <Grid item xs={12} md={6}>
-                            <TextField name="realEstateTaxes" label="Real Estate Taxes" fullWidth value={formValues.realEstateTaxes} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                            <TextField name="carInsurance" label="Car Insurance" fullWidth value={formValues.carInsurance} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                            <TextField name="houseInsurance" label="House Insurance" fullWidth value={formValues.houseInsurance} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField name="creditCard" label="All Credit Card Payment" fullWidth value={formValues.creditCard} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                            <TextField name="loanPayment" label="All Loan Payments" fullWidth value={formValues.loanPayment} onChange={handleInputChange} sx={{ marginBottom: 2 }} />
-                        </Grid>
+
+        <Container maxWidth="md" style={{ padding: 24, marginTop: 32 }}>
+            <Typography variant="h5" align="center" gutterBottom>
+                Fundamental Bill Payments
+            </Typography>
+            <Typography variant="subtitle1" align="center" gutterBottom sx={{ marginBottom: 2 }}>
+                Take some time to think about monthly bill payments you must make.
+            </Typography>
+
+            <form onSubmit={handleSubmit}>
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item xs={12} md={6}>
+                        <TextField name="realEstateTax"
+                            label="Real Estate Taxes"
+                            fullWidth
+                            value={formValues.realEstateTax}
+                            onChange={handleInputChange}
+                            sx={{ marginBottom: 2 }} />
+                        <TextField name="carInsurance"
+                            label="Car Insurance"
+                            fullWidth
+                            value={formValues.carInsurance}
+                            onChange={handleInputChange}
+                            sx={{ marginBottom: 2 }} />
+                        <TextField name="houseInsurance"
+                            label="Home Insurance"
+                            fullWidth
+                            value={formValues.houseInsurance}
+                            onChange={handleInputChange}
+                            sx={{ marginBottom: 2 }} />
                     </Grid>
-                    <ProgressBar back={'startplan'} next={'personalsavings'} value={10} budgetId={budgetId}/>
-                </form>
-            </Paper>
+                    <Grid item xs={12} md={6}>
+                        <TextField name="creditCard"
+                            label="All Credit Card Payments"
+                            fullWidth
+                            value={formValues.creditCard}
+                            onChange={handleInputChange}
+                            sx={{ marginBottom: 2 }} />
+                        <TextField name="loanPayments"
+                            label="All Loan Payments"
+                            fullWidth
+                            value={formValues.loanPayments}
+                            onChange={handleInputChange}
+                            sx={{ marginBottom: 2 }} />
+                        {formSubmitted ? (
+                            <Button type='button' onClick={handleEdit}>
+                                Edit
+                            </Button>
+                        ) : (
+                            <Button type='submit'>
+                                Submit
+                            </Button>
+                        )}
+                    </Grid>
+                </Grid>
+                <ProgressBar back={`startplan`} next={`personalsavings`} value={10} budgetId={budgetId} />
+            </form>
         </Container>
     );
 }
 
-export default PBPage2;
+export default PBpage2;
