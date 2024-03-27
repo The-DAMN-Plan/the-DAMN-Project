90a854e30efc7153b3328a06f39478e4e00b32a7
Dylan Brown <dylanbrown588@gmail.com>
2024-03-27 15:06:49 -0500

    Start the cashflow page

---
+++ src/components/CashFlow/CashFlow.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Typography, TextField, Button, Container, Grid } from '@mui/material';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import SideNav from '../Nav/SideNav';
+import ProgressBar from '../ProgressBar/ProgressBar';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+
+function CashFlow() {
+
+}
+
+export default CashFlow;
