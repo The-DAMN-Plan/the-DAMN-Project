b61262accc8f573064acc3ed64b1f6e43e24379c
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-05-08 11:52:49 -0500

    jest test config, module updates and test file changes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    testEnvironment: 'jsdom',
+    // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+    testEnvironment: 'jest-environment-jsdom',
     moduleNameMapper: {
         // Handle module aliases (if used in your project)
         '^@components/(.*)$': '<rootDir>/src/components/$1'
+    },
+    transform: {
+        // Transform files with 'babel-jest'
+        '^.+\\.[tj]sx?$': 'babel-jest'
     }
-}
+};
