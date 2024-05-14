388d1897e41838f9fe31ffb1c3955c22039c3865
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-05-14 09:57:15 -0500

    Refactor of create a plan into a modal, in progress

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Container, Grid, Paper, Typography, Button, TextField } from '@mui/material';
+import { Box, Container, Grid, Paper, Typography, Button, Modal } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Modal from '@mui/material/Modal';
-import CreateBusiness from '../CreateBusiness/CreateBusiness';
-import CustomToolTip from '../Shared/CustomToolTip';
-import Snackbar from '@mui/material/Snackbar';
+import { useHistory } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
-
+import CreateBusiness from '../CreateBusiness/CreateBusiness';
+import CreatePlanModal from '../CreatePlanModal/CreatePlanModal';
+import CustomToolTip from '../Shared/CustomToolTip';
 
 function UserPage() {
   const history = useHistory();
   const dispatch = useDispatch();
-  const budget = useSelector((store) => store.budget);
+  const budget = useSelector(store => store.budget);
   const budgetObj = budget[0];
-  const business = useSelector((store) => store.business);
+  const business = useSelector(store => store.business);
+
   const [name, setName] = useState('');
   const [pendingCreate, setPendingCreate] = useState(false);
+  const [selectedBusiness, setSelectedBusiness] = useState('');
+  const [open, setOpen] = useState(false); // For CreateBusiness modal
+  const [modalOpen, setModalOpen] = useState(false); // For CreatePlanModal
+  const [openSnack, setOpenSnack] = useState(false);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_BUSINESS' });
@@ -33,168 +33,84 @@ function UserPage() {
     if (pendingCreate) {
       history.push(`/startplan/${budgetObj.id}`);
     }
-  }, [budgetObj]);
+  }, [pendingCreate, budgetObj, history]);
 
-  const [selectedBusiness, setSelectedBusiness] = useState('');
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
   const handleChange = (event) => {
     setSelectedBusiness(event.target.value);
   };
-
-  // Snack bar Test
-  const [openSnack, setOpenSnack] = useState(false);
-
 
   const handleCloseSnack = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
-
     setOpenSnack(false);
   };
 
   const action = (
-    <React.Fragment>
-
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={handleCloseSnack}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </React.Fragment>
+    <IconButton size="small" aria-label="close" color="inherit" onClick={handleCloseSnack}>
+      <CloseIcon fontSize="small" />
+    </IconButton>
   );
 
-
   function startPlan() {
-    // Find the selected business object based on its name
-    const selectedBusinessObj = business.find(business => business.name === selectedBusiness);
-
-    // Check if a business is selected
+    const selectedBusinessObj = business.find(b => b.name === selectedBusiness);
     if (selectedBusinessObj) {
-      const budgetData = {
-        business_id: selectedBusinessObj.id, // Get the business_id of the selected business
-        name: name
-      }
-      dispatch({ type: 'START_PLAN', payload: budgetData })
+      dispatch({
+        type: 'START_PLAN',
+        payload: { business_id: selectedBusinessObj.id, name }
+      });
       setPendingCreate(true);
+      setName('');
     } else {
       console.error('No business selected for starting a plan.');
     }
-    setName('');
   }
-
-  const openVideo = () => {
-    window.open('https://youtu.be/2LaO3A_gpMQ', '_blank');
-  };
-
 
   return (
     <Container maxWidth="lg">
-      <Grid container spacing={3} sx={{ mt: 4 }}>
-        <Grid item xs={6}>
-          <Paper style={{ maxHeight: '400px', overflowY: 'auto' }}>
-            <Box p={3}>
-              <Typography variant="h3" color={'primary'} gutterBottom textAlign='center'>
-                Your Businesses
-              </Typography>
-              <Box textAlign={'right'}>
-                <CustomToolTip title="Add a business to get started" />
-              </Box>
-              {business.length === 0 ? (
-                <Typography variant="body1">
-                  You have no businesses. Please add a business.
-                </Typography>
-              ) : (
-                business.map(item => (
-                  <Typography sx={{ my: 1 }} key={item.id} variant='body1'>
-                    {item.name}
-                  </Typography>
-                ))
-              )}
-              <Button onClick={handleOpen} color='primary' variant='contained'>Create a Business</Button>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="child-modal-title"
-                aria-describedby="child-modal-description"
-              >
-                <CreateBusiness handleClose={handleClose} setOpenSnack={setOpenSnack} />
-              </Modal>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper sx={{ p: 2 }}>
+      <Grid container spacing={3} justifyContent="center" alignItems="center" sx={{ mt: 4 }}>
+        <Grid item xs={8}>
+          <Paper sx={{ maxHeight: '400px', overflowY: 'auto', p: 3 }}>
             <Typography variant="h3" color={'primary'} gutterBottom textAlign='center'>
-              Create a Plan
+              Your Businesses
             </Typography>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl fullWidth>
-                <InputLabel id="business-select-label">Select Business</InputLabel>
-                <Select
-                  labelId="business-select-label"
-                  id="business-select"
-                  label="Select Business"
-                  value={selectedBusiness}
-                  onChange={handleChange}
-                >
-                  {business.map(business => (
-                    <MenuItem key={business.id} value={business.name}>
-                      {business.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-                <TextField sx={{ my: 1 }} value={name} onChange={(e) => setName(e.target.value)} label="Plan Name" />
-              </FormControl>
+            <Box textAlign={'right'}>
+              <CustomToolTip title="Add a business to get started" />
             </Box>
-            <Box p={3}>
-              <Box display="flex" justifyContent="space-between" marginBottom="10px">
-                <Button variant="contained" color="primary" onClick={() => startPlan()}>
-                  Start a New Plan
-                </Button>
-                <Button variant="contained" color="primary" onClick={() => { history.push('/info') }}>
-                  Info
-                </Button>
-                <Button variant="contained" color="primary" onClick={openVideo}>
-                  Watch Video
-                </Button>
-              </Box>
+            {business.length === 0 ? (
+              <Typography variant="body1">
+                You have no businesses. Please add a business.
+              </Typography>
+            ) : (
+              business.map(item => (
+                <Typography sx={{ my: 1 }} key={item.id} variant='body1'>
+                  {item.name}
+                </Typography>
+              ))
+            )}
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+              <Button onClick={handleOpen} color='primary' variant='contained'>Create a Business</Button>
+              <Button onClick={handleOpenModal} color='primary' variant='contained'>Start a Damn Plan!</Button>
             </Box>
+            <Modal open={open} onClose={handleClose}>
+              <CreateBusiness handleClose={handleClose} setOpenSnack={setOpenSnack} />
+            </Modal>
           </Paper>
         </Grid>
       </Grid>
-                  {/* Test */}
-      {/* Snack Bar : Show Toast on success */}
-      <Snackbar
-          open={openSnack}
-          autoHideDuration={6000}
-          onClose={handleCloseSnack}
-          message="Business Created!"
-          action={action}
-
-        >
-          <Alert
-            onClose={handleClose}
-            severity="success"
-            variant="filled"
-            sx={{ width: '100%' }}
-          >
-            Successfully Created Business!
-          </Alert>
-        </Snackbar>
-      
+      <CreatePlanModal open={modalOpen} handleClose={handleCloseModal} businesses={business} />
+      <Snackbar open={openSnack} autoHideDuration={6000} onClose={handleCloseSnack} message="Business Created!" action={action}>
+        <Alert onClose={handleClose} severity="success" variant="filled" sx={{ width: '100%' }}>
+          Successfully Created Business!
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
